61a16097a1fbb5c847501a051ca0e7e34c5bd5fb
XuMing <ming.xu@kushuai.com>
2022-07-10 23:27:59 +0800

    setting the background color

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -7,10 +7,14 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    bg_color = (230, 230, 230)
+
     while True:
         for envent in pygame.event.get():
             if envent.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
