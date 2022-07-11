79401eb4c766528d808ab1f5b218ac1dceb7b1ee
XuMing <ming.xu@kushuai.com>
2022-07-11 22:58:07 +0800

    feat: piloting

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,7 +1,8 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
+        self.ai_settings = ai_settings
         self.screen = screen
 
         self.image = pygame.image.load('images/ship.bmp')
@@ -10,6 +11,19 @@ class Ship():
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
