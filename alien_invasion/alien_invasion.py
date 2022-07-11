744e38b8b73144a4c92b257c3c60c8244875943c
XuMing <ming.xu@kushuai.com>
2022-07-11 22:06:45 +0800

    feat: drawing the ship to the screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from ship import Ship
 from setting import Settings
 
 def run_game():
@@ -9,12 +10,15 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    ship = Ship(screen)
+
     while True:
         for envent in pygame.event.get():
             if envent.type == pygame.QUIT:
                 sys.exit()
 
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 run_game()
