7700ba76a0b7fbe1f4c4135e4410c03f70a28868
XuMing <ming.xu@kushuai.com>
2022-07-13 23:07:49 +0800

    feat: starting the game

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 from ship import Ship
 from setting import Settings
 from game_stats import GameStats
+from button import Button
 import game_functions as gf
 
 def run_game():
@@ -13,6 +14,8 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    play_button = Button(ai_settings, screen, "Play")
+
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
@@ -21,13 +24,14 @@ def run_game():
 
     stats = GameStats(ai_settings)
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+            play_button)
 
 run_game()
