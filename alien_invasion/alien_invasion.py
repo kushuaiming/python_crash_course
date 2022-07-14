8ecd1cc8d9fdd5661203c497d256e67526da7ab9
XuMing <ming.xu@kushuai.com>
2022-07-14 22:58:34 +0800

    feat: resetting the game

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -24,7 +24,8 @@ def run_game():
 
     stats = GameStats(ai_settings)
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship,
+            aliens, bullets)
 
         if stats.game_active:
             ship.update()
