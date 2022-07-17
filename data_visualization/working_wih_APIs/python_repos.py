f2b5314d676faaacf1000bb12ebe5959753afe35
XuMing <ming.xu@kushuai.com>
2022-07-17 17:41:38 +0800

    feat: visualizing repositories using pygal

--- data_visualization/working_wih_APIs/python_repos.py
+++ data_visualization/working_wih_APIs/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightStyle as LS
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
@@ -8,12 +10,15 @@ response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
-
-print("\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+my_style = LS(base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
