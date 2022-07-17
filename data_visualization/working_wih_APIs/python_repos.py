bc6b400606e42155061a21b2e7d922aa0a68ff33
XuMing <ming.xu@kushuai.com>
2022-07-17 21:26:42 +0800

    feat: finish visualizing repositories using pygal

--- data_visualization/working_wih_APIs/python_repos.py
+++ data_visualization/working_wih_APIs/python_repos.py
@@ -10,10 +10,17 @@ response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
 repo_dicts = response_dict['items']
-names, stars = [], []
+
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
 my_style = LS(base_style=LCS)
 
@@ -31,5 +38,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
