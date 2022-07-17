f913ce6b36a9657694ba1fe4300ff46e89872dd6
XuMing <ming.xu@kushuai.com>
2022-07-17 16:59:30 +0800

    feat: using web api

---
+++ data_visualization/working_wih_APIs/python_repos.py
@@ -0,0 +1,19 @@
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('Status code:', r.status_code)
+
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
