8eb643ef010884b9bd4308a47ac5b57615249a9c
XuMing <ming.xu@kushuai.com>
2022-07-17 14:29:56 +0800

    feat: add date time

--- data_visualization/downloading_data/high_lows.py
+++ data_visualization/downloading_data/high_lows.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 from matplotlib import pyplot as plt
 
@@ -7,19 +8,22 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs =[]
+    dates, highs =[], []
     for row in reader:
         try:
+            current_date = datetime.strptime(row[2], '%Y-%m-%d')
             high = int(row[8])
         except ValueError: # there may be empty data
             pass
         highs.append(high)
+        dates.append(current_date)
 
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 plt.title('Daily high temperatures, 2018', fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature(F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
