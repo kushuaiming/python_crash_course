1578654fbf26ed278c94c28b7d9fbb8c4e23a45d
XuMing <ming.xu@kushuai.com>
2022-07-17 14:33:35 +0800

    feat: add low temperature

--- data_visualization/downloading_data/high_lows.py
+++ data_visualization/downloading_data/high_lows.py
@@ -8,20 +8,23 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    dates, highs =[], []
+    dates, highs, lows =[], [], []
     for row in reader:
         try:
             current_date = datetime.strptime(row[2], '%Y-%m-%d')
             high = int(row[8])
+            low = int(row[9])
         except ValueError: # there may be empty data
             pass
+        lows.append(low)
         highs.append(high)
         dates.append(current_date)
 
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
-plt.title('Daily high temperatures, 2018', fontsize=24)
+plt.title('Daily high and low temperatures, 2018', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature(F)", fontsize=16)
